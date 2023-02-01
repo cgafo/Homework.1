e6beeb1bc97d0a156f54d110e6f10aee86cd95d5
cgafo <cgafo.ieu2021@student.ie.edu>
2023-02-01 23:52:43 +0100

    project homework

---
+++ tr.py
@@ -0,0 +1,7 @@
+"This is the main screen of the Homework"
+print("hello")
+print("hello life")
+print("hello", "world")
+print("hello", "vida", sep="")
+print("hello", "world", sep="\n")
+print("bye", end=".")
